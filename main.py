49539f56df33280f075ce62e6f9276fd76070c6b
Fazz6123 <fabio.zschenderlein@gmx.de>
2024-12-08 15:53:59 +0100

    Updates to main.py, need testing

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
+# Autologin OpnSense Python
+# v0.1
+# ---- Change to your configuration below ----
+key_url = 'http://172.16.0.254:8000/'
+status_url = key_url + "api/captiveportal/access/status/0/"
+login_url = key_url + "api/captiveportal/access/logon/0/"
+time_before_repeat = 30
+username = "peckfe"
+password = "ygYD5)3T"
+# ----
+# Only change anything below if you know what to do
+
 from time import sleep
 import requests
 
-key_url = "http://172.16.0.254:8000/"
-status_url = key_url + "api/captiveportal/access/status/0/"
-login_url = key_url + "api/captiveportal/access/logon/0/"
-username = str(input("Username: "))
-password = str(input("Password: "))
+#username = str(input("Username: "))
+#password = str(input("Password: "))
+login_status = False
+headers = {'Referer':key_url,
+           'Origin':key_url,
+           'Priority':'u=1',
+           'DNT':'1',
+           'Connection':'keep-alive'}
+payload = f"user={username}&password={password}"
+# vielleicht parameters siehe wiki
 
-headers = {'Referer':key_url,'Origin':key_url, 'Priority':'u=1'}
-payload = {'user':username, 'password':password}
+headers_status = {'Referer':key_url,
+                  'Origin':key_url,
+                  'DNT':'1',
+                  'Connection':'keep-alive'}
+payload_status = "user=&password="
+#payload_second = 'user=' + username + '&password=' + password
+#payload = {'user':username, 'password':password}
 
 while True:
-    r = requests.post(status_url, headers=headers, data=payload)
+    status_response = requests.post(status_url, headers=headers_status, data=payload_status)
+    print(status_response.json())
+    if login_status:
+         pass
+    else:
+        login_request_response = requests.post(login_url, headers=headers, data=payload)
+        print(login_request_response.json())
+    sleep(time_before_repeat)
 
-    r = requests.post(login_url, headers=headers, data=payload)
-    print(r.json())
-    sleep(30)
+#r = requests.post(status_url, headers=headers, data=payload)
