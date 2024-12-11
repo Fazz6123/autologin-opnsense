9ee6f565b47f859579b4c8c19e4e10c239ba0dd7
Fazz6123 <fabio.zschenderlein@gmx.de>
2024-12-11 18:02:12 +0100

    Add Network check, online or not

--- autologin.py
+++ autologin.py
@@ -1,21 +1,16 @@
-# Autologin OpnSense Python
-# v0.1
+# Autologin OpnSense-CaptivePortal Python
 # ---- Change to your configuration below ----
-key_url = 'http://172.16.0.254:8000/'
-status_url = key_url + "api/captiveportal/access/status/0/"
-login_url = key_url + "api/captiveportal/access/logon/0/"
-time_before_repeat = 30
-username = "peckfe"
-password = "ygYD5)3T"
+key_url = 'http://192.168.1.1/' # Ip Address of your firewall, you want to log in
+time_before_repeat = 30          # Time (seconds) between every check, if you are either connected to your firewall, or if you got logged out - default = 30
+username = "username"            # Your Username
+password = "password123!"        # And your password
 # ----
 # Only change anything below if you know what to do
 
 from time import sleep
 import requests
-
-#username = str(input("Username: "))
-#password = str(input("Password: "))
-login_status = False
+status_url = key_url + "api/captiveportal/access/status/0/"
+login_url = key_url + "api/captiveportal/access/logon/0/"
 headers = {'Referer':key_url,
            'Origin':key_url,
            'Priority':'u=1',
@@ -23,7 +18,6 @@ headers = {'Referer':key_url,
            'Connection':'keep-alive'}
 
 payload = {'user':username, 'password':password}
-
 # vielleicht parameters siehe wiki
 
 headers_status = {'Referer':key_url,
@@ -31,24 +25,36 @@ headers_status = {'Referer':key_url,
                   'DNT':'1',
                   'Connection':'keep-alive'}
 payload_status = {'user':'','password':''}
-#payload_status = "user=&password="
-#payload_second = 'user=' + username + '&password=' + password
-#payload = f"user={username}&password={password}"
+
+def user_input(error_input):
+    while True:
+        if error_input == "y" or "Y":
+            print("INFO: CONTINUE")
+            return True
+        elif error_input == "n" or "N":
+            print("INFO: EXIT")
+            return False
+        else:
+            print("INFO: INPUT DOESNT MATCH")
 
 
 
 while True:
     status_response = requests.post(status_url, headers=headers_status, data=payload_status)
     list_status_response = status_response.json()
-    print(status_response.json())
+    # print(status_response.json())
 
-    if list_status_response["clientState"] == "AUTHORIZED":
-         print("AUTHORIZED")
-         #pass
+    if status_response.status_code == 000:
+        print("INFO: NO CONNECTION")
+    elif status_response.status_code == 200:
+
+        if list_status_response["clientState"] == "AUTHORIZED":
+            pass
+        else:
+            login_request_response = requests.post(login_url, headers=headers, data=payload)
+            print(login_request_response.json()["clientState"])
     else:
-        login_request_response = requests.post(login_url, headers=headers, data=payload)
-        print(login_request_response.json())
-        print(login_request_response.json()["clientState"])
+        print(f"ERROR: UNKNOWN HTTP response: {status_response.status_code}")
+        if not user_input(str(input("Retry/Continue (y/n): "))):
+            exit(1)
     sleep(time_before_repeat)
-
-#r = requests.post(status_url, headers=headers, data=payload)
